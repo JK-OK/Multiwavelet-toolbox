01d2c2b4818a8ce5e35602c967dadc9d6e253b7e
Jozef Kromka <jozef.kromka@tuke.sk>
2022-01-25 12:52:06 +0100

    Fixed Matrix for reordered MW

--- DMWTMatrix.m
+++ DMWTMatrix.m
@@ -29,10 +29,10 @@ function mat = DMWTMatrix(n, multiwavelet, organized, varargin)
 % n: required size of transformation matrix
 % multiwavelet: multiwavelet used for creating transformation matrix
 % organized: If selected True the coeficients of a matrix will be organized
-% in following way 
+% in following way
 % c11, c11, ..., c12, c12, ..., d11, d11, ..., d12, d12, ...
 % if selected False then the coeficients of a matrix will be organized
-% in following way 
+% in following way
 % c11, c12, d11, d12, c11, c12, d11, d12, ...
 % varargin: It is possible to define custom extension according to MATLAB
 % extensions. If not defined then periodic extension will be used
@@ -52,18 +52,22 @@ for i = 1:size(mat, 2)
 
     % Select one row of eye matrix and apply DMWT on it
     vec = mat(:, i);
-    [c11, c12, d11, d12] = fast_DMWT(vec', multiwavelet,varargin{:});
+    if multiwavelet.reorder == 0
+        [c11, c12, d11, d12] = fast_DMWT(vec',multiwavelet,varargin{:});
+    else
+        [c12, d11, d12, c11] = fast_DMWT(vec',multiwavelet,varargin{:});
+    end
 
     % Organize coefficient in required way.
     if organized
         vec = [c11, c12, d11, d12]';
-%         vec = [upsample(c11, 2, 0) + upsample(c12, 2, 1), upsample(d11, 2, 0) + upsample(d12, 2, 1)];
+        %         vec = [upsample(c11, 2, 0) + upsample(c12, 2, 1), upsample(d11, 2, 0) + upsample(d12, 2, 1)];
     else
         vec = upsample(c11, 4, 0) + upsample(c12, 4, 1) + upsample(d11, 4, 2) + upsample(d12, 4, 3);
         vec = vec';
     end
 
-    % Write organized coefficients to output matrix 
+    % Write organized coefficients to output matrix
     mat(:, i) = vec;
 end
 
